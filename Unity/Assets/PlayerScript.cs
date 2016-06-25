791b3c10dc2ff78b90d33bcc0dd0e09e12abe8a8
dsi <dsi>
2016-06-25 15:54:10 +0200

    Bob is walking

--- Unity/Assets/PlayerScript.cs
+++ Unity/Assets/PlayerScript.cs
@@ -6,21 +6,31 @@ public class PlayerScript : MonoBehaviour {
 	public int speed = 50;
 	public int jumpPower = 50;
 	public bool canMove = true;
-
+    Animator animBob;
 	// Use this for initialization
 	void Start () {
-	
+        animBob = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (canMove == true) {
-			float mouveHorizontal = Input.GetAxis ("Horizontal");
-			float mouveVertical = Input.GetAxis ("Vertical");
+        float mouveHorizontal = 0;
+        float mouveVertical = 0;
+        if (canMove == true) {
+			mouveHorizontal = Input.GetAxis ("Horizontal");
+            mouveVertical = Input.GetAxis ("Vertical");
 
 			Vector2 mouvment = new Vector2 (mouveHorizontal * speed, mouveVertical * jumpPower);
 			GetComponent<Rigidbody2D> ().AddForce (mouvment);
 		}
+        if (mouveHorizontal != 0)
+        {
+            animBob.SetBool("walk", true);
+        }
+        else
+        {
+            animBob.SetBool("walk", false);
+        }
 	}
 
 	void OnCollisionEnter()
