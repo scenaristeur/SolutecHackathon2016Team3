82e9e882c89570ebfcd2cdba27b0e5d2fcec38cf
Gaëtan <gaetan.roche@etu.univ-lyon1.fr>
2016-06-25 13:24:36 +0200

    debut du deplacement du perso

---
+++ Unity/Assets/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int speed = 50;
+	public int jumpPower = 50;
+	public bool canMove = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canMove) {
+			float mouveHorizontal = Input.GetAxis ("Horizontal");
+			float mouveVertical = Input.GetAxis ("Vertical");
+
+			Vector2 mouvment = new Vector2 (mouveHorizontal * speed, mouveVertical * jumpPower);
+			GetComponent<Rigidbody2D> ().AddForce (mouvment);
+		}
+	}
+
+	void OnCollisionEnter()
+	{
+		canMove = true;
+	}
+
+	void OnCollisionExit()
+	{
+		canMove = false;
+	}
+}
