30abb4df0ee37caee19ebd78c06d1baad9203973
Gaëtan <gaetan.roche@etu.univ-lyon1.fr>
2016-06-26 13:15:09 +0200

    all unity part working

--- Unity/Assets/Scripts/BoxManager.cs
+++ Unity/Assets/Scripts/BoxManager.cs
@@ -1,31 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BoxManager : MonoBehaviour {
+public class BoxManager : ObjectManager {
 
 	public Sprite[] sprites;
-	public bool isActionable = false;
     public int level;//Niveau dans lequel se trouve la box
     public int num;//Numero de la box dans le niveau
-	private RDF database;
-	private float startTime; 
+	private bool isMovable;
+	private bool hasYellowKey;
 
 	private int gravite;
 
 	// Use this for initialization
 	void Start () {
-		string tailleBoite = "";
+		int tailleBoite = 0;
 		database = new RDF ();
-		tailleBoite = database.getValue (this.gameObject.name,"taille");
-		this.transform.localScale = new Vector2(int.Parse(tailleBoite) ,int.Parse(tailleBoite));
-		this.gravite = int.Parse(database.getValue (this.gameObject.name, "gravite"));
+		refreshData ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		float distFromPlayer = Mathf.Sqrt (Mathf.Pow(this.transform.position.x - GameObject.Find("Bob").transform.position.x, 2) 
 			+ Mathf.Pow(this.transform.position.y - GameObject.Find("Bob").transform.position.y, 2));
-		if (distFromPlayer < 3) {
+		if (distFromPlayer < this.distActivable) {
 			GetComponent<SpriteRenderer> ().sprite = sprites [1];
 			isActionable = true;
 		} else {
@@ -37,37 +34,23 @@ public class BoxManager : MonoBehaviour {
 			refreshData ();
 		
 		if (Input.GetKeyDown ("a") && isActionable) {
-			//OpenBrowser openBrowser = new OpenBrowser (this.gameObject.name);
-			//getData();
-			startTime = Time.time;
+			startTime = Time.time; 
 			StartCoroutine (OpenBrowser ());
 		}
 	}
 
-    void getData()
-    {
-        GameObject.Find("Bob").GetComponent<Animator>().SetBool("bro", true);
-        GameObject.Find("Bob").GetComponent<Animator>().SetInteger("num", num);
-        GameObject.Find("Bob").GetComponent<Animator>().SetInteger("lvl", level);
-    }
-
-
-	void refreshData(){
-		string tailleBoite = "";
+	protected override void refreshData(){
+		int tailleBoite = 0;
 		try{
-			tailleBoite = database.getValue (this.gameObject.name,"taille");
-			this.transform.localScale = new Vector2(int.Parse(tailleBoite) ,int.Parse(tailleBoite));
+			tailleBoite = int.Parse(database.getValue (this.gameObject.name,"taille"));
+			this.transform.localScale = new Vector2(tailleBoite ,tailleBoite);
+			this.gravite = int.Parse(database.getValue (this.gameObject.name, "gravite"));
+			this.distActivable = tailleBoite + 2;
+			this.isMovable = bool.Parse(database.getValue(this.gameObject.name, "deplacable"));
+			GetComponent<Rigidbody2D>().isKinematic = !this.isMovable;
 		}catch(System.FormatException ignored){
 			Debug.Log (ignored.Message);
-			Debug.Log (tailleBoite);
 		}
 	}
 
-	IEnumerator OpenBrowser (){
-		while (Time.time - this.startTime < 200) {
-			yield return null;
-		}
-		Application.OpenURL("http://192.168.101.38/solutechackathon2016team3/Watch_graph/?object=" + this.gameObject.name);
-		yield return new WaitForSeconds (3f);
-	}
 }
---
+++ Unity/Assets/Scripts/LockManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockManager : ObjectManager {
+
+
+	public float distUnlockable = 1;
+	public string color = "green";
+	public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+		database = new RDF ();
+		refreshData ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject bob = GameObject.Find ("Bob");
+		float distFromPlayer = Mathf.Sqrt (Mathf.Pow(this.transform.position.x - GameObject.Find("Bob").transform.position.x, 2) 
+			+ Mathf.Pow(this.transform.position.y - bob.transform.position.y, 2));
+		if (distFromPlayer < this.distActivable) {
+			isActionable = true;
+		}
+		if (Input.GetKeyDown ("e") && isActionable)
+			refreshData ();
+
+		if (Input.GetKeyDown ("a") && isActionable) {
+			startTime = Time.time;
+			StartCoroutine (OpenBrowser ());
+		}
+	}
+
+	protected override void refreshData(){
+		try{
+			string dbcolor = database.getValue(this.gameObject.name,"couleur");
+			if(dbcolor == "vert" || dbcolor == "jaune") {
+				this.color = dbcolor;
+			}
+			if(this.color == "vert"){
+				foreach(SpriteRenderer s in GetComponentsInChildren<SpriteRenderer> ())
+					s.sprite = sprites [0];
+			}
+			else if (this.color == "jaune") {
+				foreach(SpriteRenderer s in GetComponentsInChildren<SpriteRenderer> ())
+					s.sprite = sprites [1];
+			}
+		}catch(System.FormatException ignored){
+			Debug.Log (ignored.Message);
+		}
+	}
+}
