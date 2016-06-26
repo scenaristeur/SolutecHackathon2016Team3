4415e3d6d1ca7fab2759d9a97499de179ed68dde
J0hn <jonathan.platteau@yahoo.fr>
2016-06-26 09:59:17 +0200

    Watch_graph : Adding comments

--- Watch_Graph/catcher.php
+++ Watch_Graph/catcher.php
@@ -1,49 +1,38 @@
 <?php
+// This library is for interfacing with the RDF server
 require_once( "lib/sparqllib.php" );
 
+// This homemade function remove the predicat from the value given by RDF server
 function decode($var) {
     return (preg_match(".#.", $var) ? preg_split(".#.", $var)[1] : $var);
 }
 
+// This is our subject
 $subject = $_GET['object'];
 
+// Connection to the RFD server with database test
 $db = sparql_connect( "http://192.168.101.39:3030/test/" );
 if( !$db ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
 sparql_ns( "DC","http://www.smag0/NS/hackathonSolutec/DC#" );
 
+// This request take all the link with our subject
 $sparql = "SELECT * WHERE { DC:".$subject." ?pro ?obj } LIMIT 25";
 $result = sparql_query( $sparql );
 if( !$result ) { print sparql_errno() . ": " . sparql_error(). "\n"; exit; }
 
 $fields = sparql_field_array( $result );
 
-/*print "<p>Number of rows: ".sparql_num_rows( $result )." results.</p>";
-print "<table class='example_table'>";
-print "<tr>";
-foreach( $fields as $field )
-{
-    print "<th>$field</th>";
-}
-print "</tr>";*/
-
+// We're spliting nodes and edges (arcs because I forgot that edge is called edge...)
 $nodes = array();
 $arcs = array();
 
 while( $row = sparql_fetch_array( $result ) )
 {
-    /*
-    print "<tr>";
-    foreach( $fields as $field )
-    {
-        print "<td>". decode($row[$field]) ."</td>";
-    }*/
     $nodes[] = decode($row[$fields[1]]);
     $arcs[] = decode($row[$fields[0]]);
-    //print "</tr>";
 }
-//print "</table>";
 
-
+// Building Json for GoJs
 $json = "{ \"nodeKeyProperty\": \"id\", \"nodeDataArray\": [{ \"id\": 0, \"text\": \"".$subject."\" }";
 $i = 1;
 foreach($nodes as $node) {
@@ -58,4 +47,5 @@ foreach($arcs as $arc) {
 }
 $json = substr($json, 0, -1) . "]}";
 
+// If you see this, waw it's amazing!
 print $json;
--- Watch_Graph/index.php
+++ Watch_Graph/index.php
@@ -5,12 +5,14 @@
     <meta name="description" content="A badass visualisation of the graph data of our badass videogame" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="favicon.png" />
+
+    <!-- Bootstrap -->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="favicon.png" />
+
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 
@@ -20,20 +22,36 @@
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <!-- Library for node graph -->
     <script src="lib/go.js"></script>
+   
+    <!-- Our Javascript for the node graph with GoJs -->
     <script id="code" src="js/graph.js"></script>
+
+    <!-- Our Javascript for pushing update to RDF server -->
     <script src="js/RDF.js"></script>
+
+    <!-- A global variable need for knowing the current object used -->
     <script> window.object = "<?php print $_GET['object'] ?>";</script>
 </head>
 <body onload="init()">
-<video class="hide-for-small-only" poster="http://s3.amazonaws.com/dev.assets.neo4j.com/wp-content/uploads/20160422160353/neo4j-products-hero-video-poster.jpg" loop="" autoplay="">
+<!-- The badass video in background -->
+<video class="hide-for-small-only max" poster="http://s3.amazonaws.com/dev.assets.neo4j.com/wp-content/uploads/20160422160353/neo4j-products-hero-video-poster.jpg" loop="" autoplay="">
     <source type="video/mp4" src="//s3.amazonaws.com/media.neo4j.com/neo4j-products-hero-video-10sec.mp4">Your browser doesn't support HTML5 video tag.</source>
     Your browser doesn't support HTML5 video tag.
 </video>
+<img class="show-for-small-only" src="http://s3.amazonaws.com/dev.assets.neo4j.com/wp-content/uploads/20160422160353/neo4j-products-hero-video-poster.jpg">
+<!-- The node graph result -->
 <div id="myDiagramDiv"></div>
+
 <div>
+    <!-- This textarea invisible is needed for containing the Json of our data for the node graph.
+    The Json is generate with our PHP script. -->
     <textarea id="mySavedModel" style="width:0;height:0" hidden="hidden"><?php include 'catcher.php'; ?></textarea>
 </div>
-<<iframe style="height:0" hidden="hidden" name="hidden-form"></iframe>
+
+<!-- This iframe is here to avoid popup when we posting data -->
+<iframe style="height:0" hidden="hidden" name="hidden-form"></iframe>
 </body>
 </html>
